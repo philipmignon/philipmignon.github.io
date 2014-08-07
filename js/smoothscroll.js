dc105430dda537d43bc474ee2397e568c675a7bd
philipmignon <phillip_huynh@outlook.com>
2014-08-07 13:30:22 -0700

    Changes

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -39,3 +39,23 @@ $("a.navbar-brand[href^='#']").on('click', function(e) {
      });
 
 });
+
+$("#back-to-top a[href^='#']").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(this.hash).offset().top
+     }, 700, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+});
